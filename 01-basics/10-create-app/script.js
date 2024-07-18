d14c938745e4499641cd1f3213618e2ad34129e4
dotSality <sality33@gmail.com>
2024-07-18 23:28:28 +0200

    Solved date app

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,16 @@
-import { defineComponent, createApp } from 'vue/dist/vue.esm-bundler.js'
+import { defineComponent, createApp } from "vue/dist/vue.esm-bundler.js";
+
+const appComponent = defineComponent({
+  name: "App",
+  computed: {
+    date() {
+      return new Date().toLocaleDateString('en-US', {
+        dateStyle: "long"
+      });
+    }
+  },
+
+  template: `<div>Сегодня {{ date }}</div>`
+});
+
+createApp(appComponent).mount("#app");
