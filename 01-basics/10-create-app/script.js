bbf202cc2dce93570373f1930cc20fd4ebee9a47
dotSality <sality33@gmail.com>
2024-07-19 08:56:05 +0200

    Solved date task

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,13 @@
-import { defineComponent, createApp } from 'vue/dist/vue.esm-bundler.js'
+import { defineComponent, createApp } from "vue/dist/vue.esm-bundler.js";
+
+createApp(defineComponent({
+  name: "DateApp",
+  computed: {
+    date() {
+      return new Date().toLocaleDateString(navigator.language, { dateStyle: "long" });
+    }
+  },
+
+  template: `<div>Сегодня {{ date }}</div>`
+}))
+  .mount('#app');
