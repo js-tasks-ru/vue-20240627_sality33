819515a4fa766c11e1e8a90ffbf4dc09c79c18cb
dotSality <sality33@gmail.com>
2024-08-28 19:24:25 +0200

    fixes

--- 03-components/20-UiClock/UiClock.js
+++ 03-components/20-UiClock/UiClock.js
@@ -1,4 +1,4 @@
-import { computed, defineComponent, onMounted, onUnmounted, ref } from "vue";
+import { computed, defineComponent, onUnmounted, ref } from "vue";
 
 export default defineComponent({
   name: "UiClock",
@@ -6,20 +6,17 @@ export default defineComponent({
   setup() {
 
     const date = ref(Date.now());
-    const intervalId = ref(null);
+
+    const intervalId = setInterval(() => {
+      date.value = Date.now();
+    }, 1000);
+
     const time = computed(() => Intl.DateTimeFormat(navigator.language, {
       timeStyle: "medium"
     }).format(date.value));
 
-    onMounted(() => {
-      intervalId.value = setInterval(() => {
-        date.value = Date.now();
-      }, 1000);
-    });
-
     onUnmounted(() => {
-      clearInterval(intervalId.value);
-      intervalId.value = null;
+      clearInterval(intervalId);
     });
 
     return { time };
