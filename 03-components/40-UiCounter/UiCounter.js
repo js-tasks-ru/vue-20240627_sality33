f8914d8776ae6a96459f26f138f5b46ee66c89dd
dotSality <sality33@gmail.com>
2024-08-28 19:55:13 +0200

    solved counter

--- 03-components/40-UiCounter/UiCounter.js
+++ 03-components/40-UiCounter/UiCounter.js
@@ -1,4 +1,4 @@
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from "vue";
 import { UiButton } from '@shgk/vue-course-ui'
 import './UiCounter.css'
 
@@ -9,15 +9,46 @@ export default defineComponent({
     UiButton,
   },
 
-  setup() {
-    // Рекомендуется для практики реализовать обработку событий внутри setup, а не непосредственно в шаблоне
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+
+    min: {
+      type: Number,
+      default: 0,
+    },
+
+    max: {
+      type: Number,
+      default: Infinity,
+    }
+  },
+
+  emits: ['update:count'],
+
+  setup(props, { emit }) {
+    const countModel = computed({
+      get: () => props.count,
+      set: (value) => emit("update:count", value),
+    })
+
+    const increment = () => countModel.value += 1;
+    const decrement = () => countModel.value -= 1;
+
+    return {
+      increment,
+      decrement,
+      countModel,
+    }
   },
 
   template: `
     <div class="counter">
-      <UiButton aria-label="Decrement" disabled>➖</UiButton>
-      <span class="count" data-testid="count">3</span>
-      <UiButton aria-label="Increment">➕</UiButton>
+      <UiButton aria-label="Decrement" :disabled="count === min" @click="decrement">➖</UiButton>
+      <span class="count" data-testid="count">{{ countModel }}</span>
+      <UiButton aria-label="Increment" :disabled="count === max" @click="increment">➕</UiButton>
     </div>
   `,
 })
