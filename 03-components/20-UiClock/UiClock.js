249d493095a0976795591739adfce0a8224ef234
dotSality <sality33@gmail.com>
2024-08-28 19:06:53 +0200

    eslint

--- 03-components/20-UiClock/UiClock.js
+++ 03-components/20-UiClock/UiClock.js
@@ -1,29 +1,29 @@
 import { computed, defineComponent, onMounted, onUnmounted, ref } from "vue";
 
 export default defineComponent({
-  name: 'UiClock',
+  name: "UiClock",
 
   setup() {
 
     const date = ref(Date.now());
     const intervalId = ref(null);
     const time = computed(() => Intl.DateTimeFormat(navigator.language, {
-      timeStyle: 'medium'
-    }).format(date.value))
+      timeStyle: "medium"
+    }).format(date.value));
 
     onMounted(() => {
       intervalId.value = setInterval(() => {
-        date.value = Date.now()
-      }, 1000)
-    })
+        date.value = Date.now();
+      }, 1000);
+    });
 
     onUnmounted(() => {
       clearInterval(intervalId.value);
       intervalId.value = null;
-    })
+    });
 
-    return { time }
+    return { time };
   },
 
   template: `<div class="clock">{{ time }}</div>`,
-})
+});
